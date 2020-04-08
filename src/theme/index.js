7ba5f670d1b1e23a107c1931ca8bc625fc36c64b
rogeralbinoi <roger.albino.i@gmail.com>
2020-04-08 02:26:48 -0300

    feat(plop): add plop templates

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    fullDarkRed: "#550e33",
+    darkRed: "#9c2543",
+    lightRed: "#ef4d5a",
+    red: "#db2828",
+    orange: "#f2711c",
+    yellow: "fbbd08",
+    olive: "#b5cc18"
+  }
+}
