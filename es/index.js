0818875b9c9377869f1aee7d1459e81d78144682
rogeralbinoi <roger.albino.i@gmail.com>
2020-04-12 15:56:08 -0300

    build(button): first build with theme and button

---
+++ es/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "theme", {
+  enumerable: true,
+  get: function get() {
+    return _theme2["default"];
+  }
+});
+Object.defineProperty(exports, "Button", {
+  enumerable: true,
+  get: function get() {
+    return _Button2["default"];
+  }
+});
+
+var _theme2 = _interopRequireDefault(require("./theme"));
+
+var _Button2 = _interopRequireDefault(require("./components/Button"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
