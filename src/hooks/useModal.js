39a3f6808ca61c3be0328d88d34d237efc68afee
rogeralbinoi <roger.albino.i@gmail.com>
2020-06-04 10:45:23 -0300

    fix(modal): add params and scrolling

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -3,22 +3,28 @@ import React, { useState, useCallback } from 'react'
  * Example
  * const { isOpen, openModal, closeModal, toggleModal } = useModal(false)
  */
-const useTableOrder = (defaultIsOpen = false) => {
+const useModal = (defaultIsOpen = false) => {
   const [isOpen, setIsOpen] = useState(defaultIsOpen)
+  const [params, setParams] = useState(null)
 
-  const openModal = useCallback(() => {
+  const openModal = useCallback((params) => {
+    setParams(params)
     setIsOpen(true)
   }, [])
 
   const closeModal = useCallback(() => {
     setIsOpen(false)
+    setParams(null)
   }, [])
 
   const toggleModal = useCallback(() => {
-    setIsOpen(isOpen)
+    if (!isOpen === false) {
+      setParams(null)
+    }
+    setIsOpen(!isOpen)
   }, [isOpen])
 
-  return { isOpen, openModal, closeModal, toggleModal }
+  return { isOpen, openModal, closeModal, toggleModal, params }
 }
 
-export default useTableOrder
+export default useModal
