b07140213f95408f9962d8cf377927f0c46aae12
rogeralbinoi <roger.albino.i@gmail.com>
2020-06-10 11:53:08 -0300

    test(switch): add missing tests

--- src/components/Switch/Switch.test.js
+++ src/components/Switch/Switch.test.js
@@ -5,14 +5,20 @@ import { ThemeProvider } from 'styled-components'
 import theme from '../../theme'
 import 'jest-styled-components'
 
-test('Should render Switch', () => {
-  const ControlledSwitch = () => {
-    const [checked, setChecked] = useState(false)
-    return <Switch checked={checked} onChange={(e) => onChange(e.target.checked)} />
-  }
+test('Should render Switch when checked', () => {
   const component = renderer.create(
     <ThemeProvider theme={theme}>
-      <ControlledSwitch />
+      <Switch checked={true} onChange={(e) => onChange(e.target.checked)} />
+    </ThemeProvider>
+  )
+  let tree = component.toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+test('Should render Switch when unchecked', () => {
+  const component = renderer.create(
+    <ThemeProvider theme={theme}>
+      <Switch checked={false} onChange={(e) => onChange(e.target.checked)} />
     </ThemeProvider>
   )
   let tree = component.toJSON()
