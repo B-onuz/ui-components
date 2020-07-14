62999159011079666f970632be4b6348de614b3b
Roger Albino <roger.albino.i@gmail.com>
2020-07-14 18:54:24 -0300

    fix(contextmenu): fix context menu overflow (#30)

--- src/components/ContextMenu/ContextMenu.js
+++ src/components/ContextMenu/ContextMenu.js
@@ -4,6 +4,7 @@ import { color, space } from 'styled-system'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons'
 import Button from '../Button'
+import ContextMenuDialog from './ContextMenuDialog'
 
 const StyledContextMenu = styled.div`
   ${color}
@@ -26,91 +27,6 @@ const ContextMenuToogleButton = styled.button`
   }
 `
 
-const ContextDialogMenu = styled.div`
-  position: absolute;
-  border-radius: 12px;
-  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
-  z-index: ${({ isOpen }) => (isOpen ? 1 : -100)};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 8px 16px;
-  background: #fff 0% 0% no-repeat padding-box;
-  box-shadow: 0px 2px 6px #2c28281c;
-  transition: all 300ms;
-
-  ${({ direction }) => {
-    if (direction === 'left') {
-      return `
-      right: calc(100% + 24px);
-      top: -50%;
-    `
-    }
-    if (direction === 'right') {
-      return `
-      left: calc(100% + 24px);
-      top: -50%;
-    `
-    }
-    if (direction === 'top') {
-      return `
-      left: 50%;
-      transform: translateX(-50%);
-      bottom: calc(100% + 1.5em);
-    `
-    }
-    if (direction === 'bottom') {
-      return `
-      transform: translateX(-50%);
-      left: 50%;
-      top: calc(100% + 1.5em);
-    `
-    }
-  }}
-  &::after {
-    content: '';
-    width: 1em;
-    height: 1em;
-    position: absolute;
-    box-shadow: 1px 2px 2px -1px #2c28281c;
-    background: #fff 0% 0%;
-    ${({ direction }) => {
-      if (direction === 'left') {
-        return `
-      right: -0.5em;
-      top: 1.6em;
-      transform: rotateZ(-45deg);
-    `
-      }
-      if (direction === 'right') {
-        return `
-      left: -0.5em;
-      top: 1.6em;
-      transform: rotateZ(135deg);
-    `
-      }
-      if (direction === 'top') {
-        return `
-      top: calc(100% - 0.5em);
-      transform: rotateZ(45deg);
-      left: 0;
-      right: 0;
-      margin: auto;
-    `
-      }
-      if (direction === 'bottom') {
-        return `
-      bottom: calc(100% - 0.5em);
-      transform: rotateZ(-135deg);
-      left: 0;
-      right: 0;
-      margin: auto;
-    `
-      }
-    }}
-  }
-`
-
 const Divider = styled.hr`
   width: 100%;
   margin: 8px 0;
@@ -124,39 +40,28 @@ const Icon = styled(FontAwesomeIcon)`
 const ContextMenu = ({ children, direction, contextMenuActions, ...rest }) => {
   const [isOpen, setIsOpen] = useState(false)
   const wrapperRef = useRef(null)
+  const buttonRef = useRef(null)
 
-  const useOutsideAlerter = (ref) => {
-    useEffect(() => {
-      function handleClickOutside(event) {
-        if (ref.current && !ref.current.contains(event.target)) {
-          setIsOpen(false)
-        }
-      }
-
-      document.addEventListener('mousedown', handleClickOutside)
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside)
-      }
-    }, [ref])
+  const onClose = () => {
+    setIsOpen(false)
+    return true
   }
-
-  useOutsideAlerter(wrapperRef)
 
   return (
     <StyledContextMenu ref={wrapperRef} {...rest}>
-      <ContextMenuToogleButton onClick={() => setIsOpen(!isOpen)}>
+      <ContextMenuToogleButton ref={buttonRef} onClick={() => setIsOpen((last) => !last)}>
         <Icon icon={faEllipsisH} />
       </ContextMenuToogleButton>
-      <ContextDialogMenu isOpen={isOpen} direction={direction}>
+      <ContextMenuDialog isOpen={isOpen} onClose={onClose} buttonRef={buttonRef} direction={direction}>
         {(contextMenuActions || []).map((action, index) => (
           <>
-            <Button linkButton color={'primary'} onClick={() => action.buttonFunction()}>
+            <Button linkButton color={'primary'} onClick={() => onClose() && action.buttonFunction()}>
               {action.text}
             </Button>
             {index + 1 !== contextMenuActions.length ? <Divider /> : null}
           </>
         ))}
-      </ContextDialogMenu>
+      </ContextMenuDialog>
     </StyledContextMenu>
   )
 }
---
+++ src/components/ContextMenu/ContextMenuDialog.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import ReactDOM from 'react-dom'
+
+const Dialog = styled.div`
+  position: absolute;
+  border-radius: 12px;
+  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+  z-index: ${({ isOpen }) => (isOpen ? 1 : -100)};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 8px 16px;
+  background: #fff 0% 0% no-repeat padding-box;
+  box-shadow: 0px 2px 6px #2c28281c;
+
+  ${({ direction, top, right, left, bottom }) => {
+    if (direction === 'left') {
+      return `
+      top: ${top};
+      left: ${left};
+      transform: translateX(calc(-100% - 32px * 2  - 5px));
+    `
+    }
+    if (direction === 'right') {
+      return `
+      top: ${top};
+      left: ${left};
+    `
+    }
+    if (direction === 'top') {
+      return `
+      top: ${top};
+      left: ${left};
+      transform: translateX(calc(-50% - 32px / 2)) translateY(calc(-100% - 24px));
+    `
+    }
+    if (direction === 'bottom') {
+      return `
+      top: ${top};
+      left: ${left};
+      transform: translateX(calc(-50% - 32px / 2));
+    `
+    }
+  }}
+  &::after {
+    content: '';
+    width: 1em;
+    height: 1em;
+    position: absolute;
+    box-shadow: 1px 2px 2px -1px #2c28281c;
+    background: #fff 0% 0%;
+    ${({ direction }) => {
+      if (direction === 'left') {
+        return `
+      right: -0.5em;
+      top: 1.6em;
+      transform: rotateZ(-45deg);
+    `
+      }
+      if (direction === 'right') {
+        return `
+      left: -0.5em;
+      top: 1.6em;
+      transform: rotateZ(135deg);
+    `
+      }
+      if (direction === 'top') {
+        return `
+      top: calc(100% - 0.5em);
+      transform: rotateZ(45deg);
+      left: 0;
+      right: 0;
+      margin: auto;
+    `
+      }
+      if (direction === 'bottom') {
+        return `
+      bottom: calc(100% - 0.5em);
+      transform: rotateZ(-135deg);
+      left: 0;
+      right: 0;
+      margin: auto;
+    `
+      }
+    }}
+  }
+`
+
+const useRootDialog = () => {
+  const [$rootDialog, setRootDialog] = useState()
+  const IdName = 'contextMenu-root'
+
+  useEffect(() => {
+    const rootModal = document.getElementById(IdName)
+    if (rootModal) {
+      setRootDialog(rootModal)
+    } else {
+      const rootEl = document.createElement('div')
+      rootEl.id = IdName
+      setRootDialog(rootEl)
+      document.body.appendChild(rootEl)
+    }
+  })
+
+  return { $rootDialog }
+}
+
+const Wrapper = styled.div`
+  z-index: 99999999999;
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`
+
+const ContextMenuDialog = ({ isOpen, buttonRef, onClose, direction, ...rest }) => {
+  const { $rootDialog } = useRootDialog()
+  const [positions, setPositions] = useState({ top: 0, right: 0, bottom: 0, left: 0 })
+  const $currentButtonRef = (buttonRef || {}).current
+  const [top, setTop] = useState(0)
+  const [left, setLeft] = useState(0)
+
+  const updatePositions = () => {
+    if (!$currentButtonRef) return null
+    const offset = $currentButtonRef.getBoundingClientRect()
+
+    if (direction === 'top') {
+      setTop(`${offset.top + offset.height - 24}px`)
+      setLeft(`${offset.left + offset.width}px`)
+    }
+
+    if (direction === 'right') {
+      setTop(`${offset.top - offset.height / 2}px`)
+      setLeft(`${offset.left + offset.width + 20}px`)
+    }
+
+    if (direction === 'left') {
+      setTop(`${offset.top - offset.height / 2}px`)
+      setLeft(`${offset.left + offset.width + 20}px`)
+    }
+
+    if (direction === 'bottom') {
+      setTop(`${offset.top + offset.height + 24}px`)
+      setLeft(`${offset.left + offset.width}px`)
+    }
+  }
+
+  useEffect(() => {
+    updatePositions()
+    window.addEventListener('resize', updatePositions)
+    return () => {
+      window.removeEventListener('resize', updatePositions)
+    }
+  })
+
+  if (!$rootDialog) return null
+
+  if (!isOpen) {
+    return null
+  }
+
+  return ReactDOM.createPortal(
+    <Wrapper onClick={onClose}>
+      <Dialog direction={direction} isOpen={isOpen} top={top} left={left} {...rest} />
+    </Wrapper>,
+    $rootDialog
+  )
+}
+
+export default ContextMenuDialog
