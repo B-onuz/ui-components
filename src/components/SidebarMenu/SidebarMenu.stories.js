cb4282bba6fbabf2176eb7a70299c8db922a4342
Nicolas Garcia <60199944+nicolascgarcia@users.noreply.github.com>
2020-10-16 14:15:02 -0300

    feat(sidebar-menu): added contact button (#54)

--- src/components/SidebarMenu/SidebarMenu.stories.js
+++ src/components/SidebarMenu/SidebarMenu.stories.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import SidebarMenu from './SidebarMenu'
 import SidebarMenuItem from '../SidebarMenuItem'
+import Button from '../Button'
+import { Box, Flex } from 'reflexbox'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome, faDice, faCodeBranch, faAdjust, faKey } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faDice, faCodeBranch, faAdjust, faKey, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 export default {
   title: 'SidebarMenu',
@@ -18,7 +20,21 @@ const menuItems = [
 ]
 
 export const SidebarMenuExample = () => (
-  <SidebarMenu open={true}>
+  <SidebarMenu
+    open={true}
+    contactButton={
+      <Button style={{ backgroundColor: 'transparent' }} color="white" outline m={3}>
+        <Flex alignItems="center">
+          <Box mr={2}>
+            <FontAwesomeIcon icon={faPhone} color="#4AB77C" size="lg" />
+          </Box>
+          <Flex width={1} justifyContent="center">
+            +11 22 33333-3333
+          </Flex>
+        </Flex>
+      </Button>
+    }
+  >
     {menuItems.map((item) => {
       return <SidebarMenuItem component={'header'} icon={<FontAwesomeIcon icon={item.icon} />} children={item.text} />
     })}
@@ -26,7 +42,22 @@ export const SidebarMenuExample = () => (
 )
 
 export const SidebarMenuOnlyMobile = () => (
-  <SidebarMenu open={true} onlyMobile>
+  <SidebarMenu
+    open={true}
+    onlyMobile
+    contactButton={
+      <Button style={{ backgroundColor: 'transparent' }} color="white" outline m={3}>
+        <Flex alignItems="center">
+          <Box mr={2}>
+            <FontAwesomeIcon icon={faPhone} color="#4AB77C" size="lg" />
+          </Box>
+          <Flex width={1} justifyContent="center">
+            +11 22 33333-3333
+          </Flex>
+        </Flex>
+      </Button>
+    }
+  >
     {menuItems.map((item) => {
       return <SidebarMenuItem component={'header'} icon={<FontAwesomeIcon icon={item.icon} />} children={item.text} />
     })}
