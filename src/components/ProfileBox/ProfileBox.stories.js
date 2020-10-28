aea131ab12c77ea1fce57d6eb2a95b72ad728450
Nicolas Garcia <60199944+nicolascgarcia@users.noreply.github.com>
2020-10-28 12:13:24 -0300

    fix(profile-box): let profile box receive only some props (#64)

--- src/components/ProfileBox/ProfileBox.stories.js
+++ src/components/ProfileBox/ProfileBox.stories.js
@@ -14,8 +14,23 @@ export const ProfileBoxExample = () => (
     <NavBar style={{ marginBottom: 30 }}>
       <ProfileBox src={bnz_icon} userName="John Doe" balanceValue="2.300" />
     </NavBar>
-    <NavBar>
+    <NavBar style={{ marginBottom: 30 }}>
       <ProfileBox src={bnz_icon} iconHeight={'34px'} balanceValue="2.300" />
+    </NavBar>
+    <NavBar style={{ marginBottom: 30 }}>
+      <ProfileBox userName="John Doe" />
+    </NavBar>
+    <NavBar style={{ marginBottom: 30 }}>
+      <ProfileBox src={bnz_icon} />
+    </NavBar>
+    <NavBar style={{ marginBottom: 30 }}>
+      <ProfileBox balanceValue="2.300" />
+    </NavBar>
+    <NavBar style={{ marginBottom: 30 }}>
+      <ProfileBox userName="John Doe" balanceValue="2.300" />
+    </NavBar>
+    <NavBar>
+      <ProfileBox userName="John Doe" src={bnz_icon} />
     </NavBar>
   </>
 )
